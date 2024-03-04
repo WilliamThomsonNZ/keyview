6b0b98f320f1b2f3d6f568ec35d2208867078bf2
WilliamThomsonNZ <william__thomson@outlook.com>
2024-03-03 16:33:31 -0800

    update team logic

--- src/team-member.js
+++ src/team-member.js
@@ -1,4 +1,4 @@
-// const teamCards = document.querySelectorAll(".team-card-teampage");
+const teamCards = document.querySelectorAll(".team-card-teampage");
 
 // teamCards.forEach((card) => {
 //   const rolesText = card.querySelector(".cardposition");
@@ -13,3 +13,16 @@
 //   });
 //   rolesContainer.style.opacity = 1;
 // });
+
+const roles = document.getElementById("team-positions");
+const container = document.getElementById("positions-container");
+
+roles.textContent.split(",").forEach((role) => {
+  const roleElement = document.createElement("div");
+  roleElement.classList.add("teamrole");
+  roleElement.textContent = role;
+  container.appendChild(roleElement);
+});
+
+roles.style.display = "none";
+container.style.opacity = 1;
